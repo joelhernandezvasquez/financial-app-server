6b551169d3b14ead411f7b64c4fb8dada0a9ad3f
Joel Hernandez <jhernandez98@outlook.es>
2025-02-04 06:22:39 -0500

    add budget get endpoint

--- src/presentation/budget/controller.ts
+++ src/presentation/budget/controller.ts
@@ -1,24 +1,23 @@
 import { Request, Response } from "express";
-
+import { BudgetService } from "../services/budgets.service";
+import { CustomError } from "../../domain/errors/custom.error";
 
 export class BudgetController {
     
-    //constructor(private readonly categoryService:CategoryService){}
+    constructor(private readonly budgetService:BudgetService){}
 
-    // private handleError = (error:unknown,res:Response) =>{
-    //    if(error instanceof CustomError){
-    //      return res.status(error.statusCode).json({error:error.message})
-    //    }
-    //    console.log(`${error}`);
-    //    return res.status(500).json({error:'Internal Server Error'});
-    // }
+    private handleError = (error:unknown,res:Response) =>{
+       if(error instanceof CustomError){
+         return res.status(error.statusCode).json({error:error.message})
+       }
+       console.log(`${error}`);
+       return res.status(500).json({error:'Internal Server Error'});
+    }
 
     getBudgets = async(req:Request,res:Response) => {
-       return res.status(200).json('Get Budgets');
-    
-        // this.categoryService.getCategories(paginationDTO!)
-        // .then(categories => res.json(categories))
-        // .catch(error => this.handleError(error,res))
+        this.budgetService.getBudgets()
+        .then(budgets => res.json(budgets))
+        .catch(error => this.handleError(error,res))
       }
 
     createBudget = async(req:Request,res:Response) =>{
